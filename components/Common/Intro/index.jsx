b33ff461011fc0a7cb35ce61e425f244a2f7eed1
Vivek Sai <113660738+VivekSai07@users.noreply.github.com>
2024-04-21 22:29:07 +0530

    Add files via upload

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Vivek from '../../../public/images/vivek.JPG';
+import Vivek from '../../../public/images/vivek.png';
 
 const Intro = () => {
   return (
