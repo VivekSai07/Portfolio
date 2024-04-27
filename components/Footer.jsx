698909e36d91be3cc37d006c0c8b06110edb460b
Vivek Sai <113660738+VivekSai07@users.noreply.github.com>
2024-04-27 17:02:03 +0530

    Add files via upload

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
                     </div>
                     <span>2024 All Rights Reserved.</span>
                 </div>
-                <div className="flex items-center">
-                    <a href='https://www.fiverr.com/codeworthy' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Vivek</span></a>
-                </div>
+                {/* <div className="flex items-center">
+                    <a href='' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Vivek</span></a>
+                </div> */}
                 <div className="hidden md:flex items-center">
                     <div className="mr-1 text-base">
                         <MdMail />
